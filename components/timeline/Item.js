d2b87e83527e1c1f39427e239937f0be58bba1d7
op <you@examples.com>
2022-02-09 10:27:32 -0800

    item style tweaks

--- components/timeline/Item.js
+++ components/timeline/Item.js
@@ -2,6 +2,50 @@ import React, {useState} from "react";
 import {Flex, Box, Heading, Text, Image, useColorModeValue} from "@chakra-ui/react";
 import {motion, LayoutGroup} from "framer-motion";
 
+const ItemMedia = ({itemData, orderOnLg}) => {
+	{
+		/* MEDIA */
+	}
+	return (
+		<Flex
+			direction={["column"]}
+			order={{base: 1, lg: orderOnLg}}
+			w={{base: "100%", lg: "50%"}}
+			m={0}
+			p=".56rem"
+			justifyContent="center"
+			onClick={() => openMedia(itemData)}
+			style={{overflow: "hidden"}}
+		>
+			{itemData.vidUrl ? (
+				<iframe
+					width="100%"
+					height="260rem"
+					src={itemData.vidUrl.includes("/watch?v=") ? itemData.vidUrl.replace(".com/watch?v=", "-nocookie.com/embed/") : itemData.vidUrl}
+					frameBorder="0"
+					allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture;nocookie"
+					allowFullScreen
+				></iframe>
+			) : (
+				<Image objectFit="cover" src={itemData.imgUrl} h="18.2rem" minW="100%" alt="uploaded pic" zIndex={2} />
+			)}
+		</Flex>
+	);
+};
+const ItemArticle = ({itemData, orderOnLg, colorA, colorB}) => {
+	return (
+		<Box px="2.8rem" py="2.2rem" m={0} w={{base: "100%", lg: "50%"}} lineHeight="2.2rem" textAlign="left" order={{base: 2, lg: orderOnLg}}>
+			<Text overflowWrap display="inline" fontWeight="extrabold" color={colorA} fontSize="1.96rem">
+				{itemData.title}&nbsp;
+			</Text>
+			<Text display="inline">&#32;&#32;&#32;&#32;</Text>
+			<Text overflowWrap display="inline" color={colorB}>
+				{itemData.text}
+			</Text>
+		</Box>
+	);
+};
+
 const ItemPerYear = ({itemData}) => {
 	const modeNeutralBgColor = useColorModeValue("black", "white");
 	const neutralTextColorA = useColorModeValue("white", "black");
@@ -25,39 +69,8 @@ const ItemPerPersonal = ({itemData, modeBgColor, openMedia}) => {
 	return (
 		itemData.markType == "personal" && (
 			<Flex direction={{base: ["column"], lg: ["row"]}} bg={modeBgColor} mb="4.76rem" h="100%" w="100%">
-				{/* MEDIA */}
-				<Flex
-					direction={["column"]}
-					order={{base: 1, lg: 1}}
-					w={{base: "100%", lg: "50%"}}
-					m={0}
-					p=".56rem"
-					justifyContent="center"
-					onClick={() => openMedia(itemData)}
-					style={{overflow: "hidden"}}
-				>
-					{itemData.vidUrl ? (
-						<iframe
-							width="100%"
-							height="260rem"
-							src={itemData.vidUrl.includes("/watch?v=") ? itemData.vidUrl.replace(".com/watch?v=", "-nocookie.com/embed/") : itemData.vidUrl}
-							frameBorder="0"
-							allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture;nocookie"
-							allowFullScreen
-						></iframe>
-					) : (
-						<Image objectFit="cover" src={itemData.imgUrl} h="18.2rem" minW="100%" alt="uploaded pic" zIndex={2} />
-					)}
-				</Flex>
-				<Box px="2.8rem" py="2.8rem" m={0} w={{base: "100%", lg: "50%"}} lineHeight="2.8rem" textAlign="left" order={{base: 2, lg: 2}}>
-					<Text overflowWrap display="inline" fontWeight="extrabold" color={personalTextColorA} fontSize="1.96rem">
-						{itemData.title}&nbsp;
-					</Text>
-					<Text display="inline">&#32;&#32;&#32;&#32;</Text>
-					<Text overflowWrap display="inline" color={personalTextColorB}>
-						{itemData.text}
-					</Text>
-				</Box>
+				<ItemMedia {...{itemData, orderOnLg: 1}} />
+				<ItemArticle {...{itemData, orderOnLg: 2, colorA: personalTextColorA, colorB: personalTextColorB}} />
 			</Flex>
 		)
 	);
@@ -69,17 +82,10 @@ const ItemPerProject = ({itemData, modeBgColor, openMedia}) => {
 	return (
 		itemData.markType == "project" && (
 			<Flex direction={{base: ["column"], lg: ["row"]}} bg={modeBgColor} mb="4.76rem" h="100%" w="100%">
-				<Box px="2.8rem" py="2.8rem" m={0} w={{base: "100%", lg: "50%"}} lineHeight="2.8rem" textAlign="right" order={{base: 2, lg: 1}}>
-					<Text overflowWrap display="inline" fontWeight="extrabold" color={projectTextColorA} fontSize="1.96rem">
-						{itemData.title}&nbsp;
-					</Text>
-					<Text display="inline">&#32;&#32;&#32;&#32;</Text>
-					<Text overflowWrap display="inline" color={projectTextColorB}>
-						{itemData.text}
-					</Text>
-				</Box>
+				<ItemArticle {...{itemData, orderOnLg: 1, colorA: projectTextColorA, colorB: projectTextColorB}} />
+				<ItemMedia {...{itemData, orderOnLg: 2}} />
 				{/* MEDIA */}
-				<Box
+				{/* <Box
 					direction={["column"]}
 					order={{base: 1, lg: 2}}
 					w={{base: "100%", lg: "50%"}}
@@ -101,33 +107,32 @@ const ItemPerProject = ({itemData, modeBgColor, openMedia}) => {
 					) : (
 						<Image objectFit="cover" src={itemData.imgUrl} h="18.2rem" minW="100%" alt="uploaded pic" zIndex={2} />
 					)}
-				</Box>
+				</Box> */}
 			</Flex>
 		)
 	);
 };
 
+const itemAnim = {
+	hiding: {
+		opacity: 0,
+		// scale: 0,
+		y: -160,
+	},
+	showing: {
+		opacity: 1,
+		// scale: 1,
+		y: 0,
+		transition: {
+			type: "spring",
+			ease: "easeIn",
+			damping: 11,
+			duration: 0.17,
+		},
+	},
+};
 const Item = ({itemData, openMedia}) => {
 	const modeBgColor = useColorModeValue("pink.400", "pink.200");
-
-	const itemAnim = {
-		hiding: {
-			opacity: 0,
-			// scale: 0,
-			y: -160,
-		},
-		showing: {
-			opacity: 1,
-			// scale: 1,
-			y: 0,
-			transition: {
-				type: "spring",
-				ease: "easeIn",
-				damping: 11,
-				duration: 0.17,
-			},
-		},
-	};
 
 	return (
 		<motion.div variants={itemAnim}>
